406cbf62c49b7bdf12c2356e25bb6d6babe857c2
Gijs Nijholt <gijs.nijholt@gmail.com>
2019-05-29 15:34:56 +0200

    Update workout.py

--- workout.py
+++ workout.py
@@ -1,27 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import random
+from random import choice, randint
 import os
+import time
+
+WAIT_INTERVAL = 3
 
 def notify(title, text):
     os.system("""
-              /usr/local/bin/terminal-notifier -title "{}" -message "{}" -sound "Purr" 
+              /usr/local/bin/terminal-notifier -title "{}" -message "{}" -sound "Glass" 
               """.format(title, text))
+    time.sleep(10)
+    os.system("""
+              /usr/local/bin/terminal-notifier -title "{}" -message "{}" -sound "Frog" 
+              """.format("3", "3"))    
+    time.sleep(WAIT_INTERVAL)
+    os.system("""
+              /usr/local/bin/terminal-notifier -title "{}" -message "{}" -sound "Frog" 
+              """.format("2", "2"))        
+    time.sleep(WAIT_INTERVAL)
+    os.system("""
+              /usr/local/bin/terminal-notifier -title "{}" -message "{}" -sound "Frog" 
+              """.format("1", "1"))        
+    time.sleep(WAIT_INTERVAL)
+    os.system("""
+              /usr/local/bin/terminal-notifier -title "{}" -message "{}" -sound "Frog" 
+              """.format("Go! Perform the mini workout", text))        
+    time.sleep(WAIT_INTERVAL)
+    os.system("pmset displaysleepnow")
+
 
 workouts = [
-    "10 pushups",
-    "30 second hollow hold",
-    "30 second plank hold",
-    "10 airsquats",
-    "2 pike walks",
-    "10 lunge steps",
-    "10 sit ups",
-    "30 second side plank (pick a side)",
-    "10 burpees",
-    "10 second L-sit in chair",
+    "{} pushups".format(randint(5,20)),
+    "{} airsquats".format(randint(5,20)),
+    
+    "{} second hollow hold".format(randint(10,30)),
+    "{} second plank hold".format(randint(10,30)),
+    "{} second side plank hold".format(randint(10,15)),
+    
+    "{} pike walks".format(randint(2,10)),
+    "{} lunges".format(randint(2,10)),
+    "{} sit-ups".format(randint(5,20)),
+
+    "{} burpees".format(randint(5,20)),
+    "{} second L-sit (in chair or on the ground)".format(randint(5,10)),
 ]
 
-selected_workout = random.choice(workouts)
+selected_workout = choice(workouts)
 
 notify("💪🏻  Time to work out!", selected_workout)
